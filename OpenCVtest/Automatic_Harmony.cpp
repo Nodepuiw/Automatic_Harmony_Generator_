74e7da918b17f86b9aef7ab0c70af747b4071250
Nodepuiw <endonoriaki.3314@gmail.com>
2017-06-28 17:38:36 +0900

    _0628_

--- OpenCVtest/Automatic_Harmony.cpp
+++ OpenCVtest/Automatic_Harmony.cpp
@@ -5,8 +5,16 @@ using namespace cv;
 using namespace std;
 
 void exclude_lines(const Mat& _input_score, Mat* _output_score) {
-	//---- 楽譜から縦線と横線を除く処理（先行研究あり）
-	*_output_score = _input_score;
+	//---- 楽譜から縦線を除く処理（先行研究あり)
+	Sobel(_input_score, *_output_score, CV_32F, 0, 1);
+	threshold(*_output_score, *_output_score, 100, 255, THRESH_BINARY_INV);
+	imshow("without_vertical_lines", *_output_score);
+	waitKey(0);
+
+	Sobel(*_output_score, *_output_score, CV_32F, 1, 0);
+	threshold(*_output_score, *_output_score, 100, 255, THRESH_BINARY_INV);
+	imshow("without_horizontal_lines", *_output_score);
+	waitKey(0);
 }
 
 void extract_notes(const Mat& _input_noline_score, int* notes_array) {
@@ -22,15 +30,25 @@ void optimal_harmonic_notes(char* code_name, int _input_note, int* _output_note)
 }
 
 int main() {
-	Mat score = imread("./data/notes.png");
+	Mat score = imread("./data/score_2.png");
 	Mat score2;
+	Mat gray_score;
+	Mat gauss_score;
 
 	imshow("Original_score", score);
 	waitKey(0);
 
-	exclude_lines(score, &score2);
-	imshow("Score_with_no_lines", score2);
+	//---- グレースケール化
+	threshold(score, gray_score, 100, 255, THRESH_BINARY);
+	imshow("Gray_score", gray_score);
 	waitKey(0);
+	//----
+
+	//---- gaussian filter 
+
+	//---- 
+
+	exclude_lines(gray_score, &score2);
 
 
 	return 0;
